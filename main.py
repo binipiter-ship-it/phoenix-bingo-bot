e68f5a5679ae9d5565ac66cd0f08a3e5b44da7cd
binipiter-ship-it <binipiter@gmail.com>
2026-08-03 17:56:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+import os
+import threading
+import http.server
+import socketserver
 import telebot
 
-# ከ BotFather ያገኙትን ቶከን ከታች ባለው ቦታ ያስገቡ
+# የራስዎን የ BotFather ቶከን እዚህ በትክክል ያስገቡ
 TOKEN = '8970603591:AAGWxqJ7Kfu_r2R5d8VjMstT2jt7R2ua-fI'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "ሰላም! ወደ ፊኒክስ ቢንጎ (Phoenix Bingo) እንኳን በደህና መጡ! 🎮")
+    bot.reply_to(message, "ሰላም! ወደ ፊኒክс ቢንጎ (Phoenix Bingo) እንኳን በደህና መጡ! 🎮")
+
+# Render የሚጠይቀውን ፖርት (Port) ክፍት የሚያደርግ ትንሽ ሰርቨር
+PORT = int(os.environ.get("PORT", 10000))
+
+class HealthCheckHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive and running!")
+
+def run_server():
+    with socketserver.TCPServer(("", PORT), ) as httpd:
+        print(f"Serving HTTP on port {PORT}")
+        httpd.serve_forever()
+
+# ሰርቨሩን ከበስተጀርባ (Background) እናስጀምራለን
+server_thread = threading.Thread(target=run_server, daemon=True)
+server_thread.start()
 
 print("ሰርቨሩ ስራ ጀምሯል... ቦቱ መልእክት እየጠበቀ ነው!")
 bot.infinity_polling()
